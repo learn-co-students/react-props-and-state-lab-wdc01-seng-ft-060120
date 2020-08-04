bf4b1c64cd811c54ef1f025129fc1bcf8a9c8d65
devdave0 <dbchung24@gmail.com>
2020-08-04 16:20:25 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -15,19 +15,50 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = ({target: { value }}) => { 
+    this.setState({
+      filters: {...this.state.filters, type: value
+      }
+    })
+  }
+
+  onFindPetsClick = () => {
+    let url = '/api/pets';
+
+    if (this.state.filters.type !== 'all'){
+      url += `?type=${this.state.filters.type}`
+    }
+    
+    fetch(url)
+    .then(resp => resp.json())
+    .then(returnValues => {
+      this.setState({pets: returnValues})
+    })
+  }
+
+  onAdoptPet = (id) => {
+    const newPet = this.state.pets.map(pet => {
+      return pet.id === id ? {...pet, isAdopted: true} : pet//if true set's isAdopted prop to :true, if not then returns just the pet?
+    })
+    this.setState({pets: newPet})
+  }
+
   render() {
     return (
       <div className="ui container">
         <header>
           <h1 className="ui dividing header">React Animal Shelter</h1>
+          
         </header>
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.onChangeType}
+              onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
