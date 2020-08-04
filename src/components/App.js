cee7debeb19cc0a094dad087861d3cbf4b36bafb
Marcos Velasco <devslife7@gmail.com>
2020-08-04 17:46:04 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,83 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: "all",
+      },
+    };
   }
+
+  updateState = (data, type) => {
+    this.setState({
+      pets: [...data],
+      filters: {
+        type: type,
+      },
+    });
+  };
+
+  onChangeType = (event) => {
+    this.setState({
+      filters: {
+        type: event.target.value,
+      },
+    });
+  };
+
+  onFindPetsClick = () => {
+    console.log("button clicked");
+
+    let type = this.state.filters.type;
+
+    if (type === "cat") {
+      type = "?type=cat";
+    } else if (type === "dog") {
+      type = "?type=dog";
+    } else if (type === "micropig") {
+      type = "?type=micropig";
+    } else {
+      type = "";
+    }
+
+    fetch("/api/pets" + type)
+      .then((resp) => resp.json())
+      .then((pets) => {
+        this.setState({
+          pets: [...pets],
+        });
+      });
+  };
+
+  onAdoptPet = (petId) => {
+    console.log("this is the pet id:", petId);
+    console.log("PETS BEFORE MAP", this.state.pets);
+    // debugger;
+
+    let updatedPetsList = this.state.pets.map((pet) => {
+      if (pet.id === petId) {
+        // change the boolean
+        pet.isAdopted = true;
+        return pet;
+      } else {
+        return pet;
+      }
+    });
+
+    console.log("PETS AFTER MAP", updatedPetsList);
+
+    this.setState({
+      pets: updatedPetsList,
+    });
+    // debugger;
+  };
 
   render() {
     return (
@@ -24,16 +88,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
